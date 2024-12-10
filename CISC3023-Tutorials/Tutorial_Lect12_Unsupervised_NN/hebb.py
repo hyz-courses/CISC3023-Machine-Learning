80fb4db2f6bb3409dda921e1d543cedcf9261913
Yanzhen Huang <huangyanzhen0108@163.com>
2024-12-10 12:25:12 +0800

    Update hebb.py

--- CISC3023-Tutorials/Tutorial_Lect12_Unsupervised_NN/hebb.py
+++ CISC3023-Tutorials/Tutorial_Lect12_Unsupervised_NN/hebb.py
@@ -16,8 +16,8 @@ r = 5  # Five neurons in the input layer
 s = 5  # Five neurons in the output layer
 [r, Q] = p.shape
 
-w = np.eye(s)
-b = np.random.rand(s, 1)
+w = np.eye(s)   # 5x5 identity matrix. row = neuron.
+b = np.random.rand(s, 1)    # Random vector with length of 5
 
 max_epoch = 1000  # Maximum number of epochs
 lr = 0.1         # Learning rate
@@ -25,12 +25,17 @@ dr = 0.02        # Decay rate or forgetting factor
 
 for epoch in range(1, max_epoch+1):
     for q in range(Q):
-        # Presentation phase
-        a = np.heaviside(np.dot(w, p[:, q]).reshape(-1, 1) - b, 0)
-        # Learning phase
-        dw = lr * np.outer(a, p[:, q]) - dr * w
+        # Presentation phase: Neuron Activation, length-5 vector
+        activation = np.heaviside(np.dot(w, p[:, q]).reshape(-1, 1) - b, 0)
+
+        # Learning phase: Update weights with Hebb's Law
+        dw = lr * np.outer(activation, p[:, q]) - dr * w
         w = w + dw
 
 p = np.array([[1], [0], [0], [0], [1]])
-a = np.heaviside(np.dot(w, p).reshape(-1, 1) - b, 0)
+activation = np.heaviside(np.dot(w, p).reshape(-1, 1) - b, 0)
 
+for weights in w:
+    print(weights.astype(np.float32))
+
+print(f"Prediction res:\n {activation}")
